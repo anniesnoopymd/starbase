f403b9231c423017037f9472377b3722d6018b09
anniesnoopymd <anniesnoopymd@gmail.com>
2017-07-08 20:36:19 +0800

    create App.vue

---
+++ starbase/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id='app'>
+        <h3>{{title}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            title: 'Generate Your Team'
+
+        }
+    }
+
+}
+</script>
